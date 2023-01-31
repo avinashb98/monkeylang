9ae59ce4500fed155506c76b37b431bb331f9a6c
Avinash Bhardwaj <avinashb97@gmail.com>
2023-01-31 10:02:56 +0530

    add repl

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"monkeylang/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
